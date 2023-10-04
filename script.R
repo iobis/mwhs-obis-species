8609a6832815fac1567e17420bd06ddcbad3ff27
Pieter Provoost <pieterprovoost@gmail.com>
2023-10-04 10:05:38 +0200

    use coordinate uncertainty

--- script.R
+++ script.R
@@ -7,88 +7,104 @@ library(h3jsr)
 library(stringi)
 library(jsonlite)
 library(glue)
+library(robis)
+library(tidyr)
+library(nngeo)
 
 # configuration
 
-res <- 7
-export_file <- "~/Desktop/temp/obis_20230726.parquet"
+h3_res <- 7
 shapefile <- "https://github.com/iobis/mwhs-shapes/raw/master/output/marine_world_heritage.gpkg"
-
-# index occurrences and load into sqlite
-
-row_to_geo <- function(row) {
-  geo_to_h3(c(row$decimalLatitude, row$decimalLongitude), res)
-}
-
-reader <- open_dataset(export_file) %>%
-  select(decimalLongitude, decimalLatitude, phylum, class, order, family, genus, species, AphiaID, date_year, redlist_category) %>%
-  as_record_batch_reader()
-
-if (file.exists("temp.db")) {
-  file.remove("temp.db")
-}
-con <- dbConnect(RSQLite::SQLite(), "temp.db")
-
-while (TRUE) {
-  batch <- reader$read_next_batch()
-  if (is.null(batch)) {
-    break
-  }
-  batch %>%
-    as.data.frame() %>%
-    mutate(h3 = row_to_geo(.)) %>%
-    group_by(h3, phylum, class, order, family, genus, species, AphiaID, redlist_category) %>%
-    summarize(records = n(), max_year = max(date_year, na.rm = TRUE)) %>%
-    dbWriteTable(con, "occurrence", ., append = TRUE)
-}
-
-# index site shapes and load into sqlite
-
-sites <- read_sf(shapefile) %>%
-  mutate(
-    name_simplified = gsub("_+", "_", gsub("[^[:alnum:]]", "_", tolower(stri_trans_general(name, "latin-ascii"))))
-  )
-polys <- polygon_to_cells(sites, res)
-sites_h3 <- sites %>%
-  st_drop_geometry() %>%
-  select(name_simplified) %>%
-  mutate(h3 = polys) %>%
-  tidyr::unnest(h3)
-
-dbWriteTable(con, "sites", sites_h3, append = FALSE, overwrite = TRUE)
-
-# query sqlite
-
-df <- dbGetQuery(con, "select phylum, class, `order`, family, genus, species, AphiaID, redlist_category, sites.name_simplified, sum(records) as records, max(max_year) as max_year from occurrence left join sites on sites.h3 = occurrence.h3 group by phylum, class, `order`, family, species, redlist_category, sites.name_simplified") %>%
-  filter(!is.na(name_simplified) & !is.na(species) & !is.na(phylum)) %>%
-  mutate(max_year = ifelse(is.infinite(max_year), NA, max_year))
-
+sf_use_s2(FALSE)
+st <- storr::storr_rds("storr")
 fish_classes <- c("Actinopteri", "Cladistii", "Coelacanthi", "Elasmobranchii", "Holocephali", "Myxini", "Petromyzonti", "Teleostei")
 turtle_orders <- c("Testudines")
 mammal_classes <- c("Mammalia")
 
-scope <- df %>%
-  mutate(
-    group = case_when(
-      class %in% fish_classes ~ "fish",
-      order %in% turtle_orders ~ "turtle",
-      class %in% mammal_classes ~ "mammal"
-    )
-  ) %>%
-  filter(!is.na(group) & species != "Homo sapiens")
+# read shapefile
 
-site_names <- unique(sites$name_simplified)
+sites <- read_sf(shapefile) %>%
+  mutate(name_simplified = gsub("_+", "_", gsub("[^[:alnum:]]", "_", tolower(stri_trans_general(name, "latin-ascii"))))) %>%
+  group_by(name_simplified) %>%
+  summarize()
 
-for (site in site_names) {
-  site_list <- scope %>%
-    filter(name_simplified == site) %>%
-    select(-name_simplified) %>%
-    arrange(group, phylum, class, order, species)
+# get occurrences from API
+
+occ_for_geom <- function(geom) {
+  wkt <- st_as_text(st_as_sfc(st_bbox(geom)), digits = 6)
+  occ <- occurrence(geometry = wkt, fields = c("decimalLongitude", "decimalLatitude", "coordinateUncertaintyInMeters", "date_year", "phylum", "class", "order", "family", "genus", "species", "aphiaID", "category"))
+  return(occ)
+}
+
+for (site_name in sites$name_simplified) {
+
+  message(site_name)
+  site <- sites %>% filter(name_simplified == site_name)
+  
+  if (!st$exists(site_name)) {
+
+    # get occurrences after applying buffer and bounding box
+    
+    site_buffered_bbox <- st_as_sfc(site %>% st_buffer(1) %>% st_bbox()) 
+
+    occ <- occ_for_geom(site_buffered_bbox) %>%
+      filter(!is.na(species)) %>%
+      rename(c("redlist_category" = "category", "AphiaID" = "aphiaID"))
+    
+    occ_grouped <- occ %>%
+      group_by(decimalLongitude, decimalLatitude, phylum, class, order, family, genus, species, AphiaID, redlist_category) %>%
+      summarize(
+        records = n(),
+        max_year = max(date_year, na.rm = TRUE),
+        coordinateUncertaintyInMeters = max(as.numeric(coordinateUncertaintyInMeters), na.rm = TRUE)
+      ) %>%
+      ungroup()
+
+    # calculate distance from points to geometry
+    
+    site_noholes <- site %>% st_remove_holes()
+
+    points <- occ_grouped %>%
+      group_by(decimalLongitude, decimalLatitude) %>%
+      summarize() %>%
+      st_as_sf(coords = c("decimalLongitude", "decimalLatitude"), crs = 4326, remove = FALSE) %>%
+      mutate(distance = as.numeric(st_distance(geometry, site_noholes))) %>%
+      st_drop_geometry()
+  
+    occ_grouped <- occ_grouped %>%
+      left_join(points, by = c("decimalLongitude", "decimalLatitude")) %>%
+      mutate_at(vars(coordinateUncertaintyInMeters), ~ if_else(is.na(.) | is.infinite(.), 10, .))
+
+    species <- occ_grouped %>%
+      filter(distance <= coordinateUncertaintyInMeters) %>%
+      group_by(phylum, class, order, family, genus, species, AphiaID, redlist_category) %>%
+      summarize(
+        records = sum(records),
+        max_year = max(max_year, na.rm = TRUE)
+      ) %>%
+      mutate(max_year = ifelse(is.infinite(max_year), NA, max_year)) %>%
+      mutate(
+        group = case_when(
+          class %in% fish_classes ~ "fish",
+          order %in% turtle_orders ~ "turtle",
+          class %in% mammal_classes ~ "mammal"
+        )
+      ) %>%
+      filter(!is.na(group) & species != "Homo sapiens")
+    
+    st$set(site_name, species)
+
+  }
+  
+}
+
+for (site_name in sites$name_simplified) {
+  site_list <- st$get(site_name)
   json = toJSON(list(
     created = unbox(strftime(as.POSIXlt(Sys.time(), "UTC"), "%Y-%m-%dT%H:%M:%S")),
     species = site_list
   ), pretty = TRUE)
-  write(json, glue("lists/{site}.json"))
+  write(json, glue("lists/{site_name}.json"))
 }
 
-write(toJSON(site_names), "lists/sites.json")
+write(toJSON(sites$name_simplified), "lists/sites.json")
